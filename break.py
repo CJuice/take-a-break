16f7a36db25de35cf76113f84fa4abe7c0ca3c0b
CJuice <CJuice@users.noreply.github.com>
2017-10-03 13:06:26 -0500

    updated code for shorter break interval

--- break.py
+++ break.py
@@ -4,14 +4,15 @@
 # run a maximum of 3 times throughout the day.
 
 # author: john haney
+# revised by CJuice 9/13/17
 
 import time, webbrowser
 
 break_count = 0
 
 print("Program started at: " + time.ctime())
-while(break_count < 3):
-    time.sleep(7200)
+while(break_count < 18):
+    time.sleep(1800)
     webbrowser.open("http://media.riffsy.com/images/b35382f7ba569d6c6b3620889207c1cf/tenor.gif")
     break_count = break_count + 1
 
